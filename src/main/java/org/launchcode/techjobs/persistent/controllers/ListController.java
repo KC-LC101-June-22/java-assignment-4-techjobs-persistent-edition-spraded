209651338e551e5a22766cac51f6cce7f5122727
Emily Spradling <spradling.ed@gmail.com>
2022-12-04 19:18:15 -0600

    tests passing

--- src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
@@ -42,7 +42,7 @@ public class ListController {
     public String list(Model model) {
         model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute("skills", skillRepository.findAll());
-        model.addAttribute("jobs", jobRepository.findAll());
+//        model.addAttribute("jobs", jobRepository.findAll());
         return "list";
     }
 
